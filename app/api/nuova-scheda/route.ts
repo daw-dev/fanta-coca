a2f2d557af8a054e022f953d35d0a13b3247e8b3
daw-dev <dawdev04@gmail.com>
2024-09-26 20:54:46 +0200

    commented unused stuff

--- app/api/nuova-scheda/route.ts
+++ app/api/nuova-scheda/route.ts
@@ -1,17 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
-import schemaScheda from "@/utils/schema-scheda.json";
-import Ajv from "ajv";
-import addFormats from "ajv-formats";
+// import schemaScheda from "@/utils/schema-scheda.json";
+// import Ajv from "ajv";
+// import addFormats from "ajv-formats";
 import { connectToDatabase } from "@/lib/mongodb";
 import { Scheda } from "@/lib/types";
 
-const ajv = new Ajv();
-addFormats(ajv);
-const validate = ajv.compile(schemaScheda);
+// const ajv = new Ajv();
+// addFormats(ajv);
+// const validate = ajv.compile(schemaScheda);
 
 export async function POST(request: NextRequest) {
   const scheda = await request.json();
-  const isValid = validate(scheda);
+  // const isValid = validate(scheda);
 
   // if (!isValid)
   //   return NextResponse.json(
